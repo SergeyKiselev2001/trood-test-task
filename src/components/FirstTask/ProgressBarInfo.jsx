af26da29a7a031f464629a3d0ead669ea391c45d
Segrey Kiselev <kissergey98@mail.ru>
2022-09-21 22:50:53 +0300

    fix bugs and add task switch logic

--- src/components/FirstTask/ProgressBarInfo.jsx
+++ src/components/FirstTask/ProgressBarInfo.jsx
@@ -1,23 +1,19 @@
 import classes from './../../styles/first.module.css'
 
 const ProgressBarInfo = ({ items }) => {
-    const valueSum = items.reduce(
-        (prev, current) => (prev += current.value),
-        0
-    );
+    const valueSum = items.reduce((prev, current) => (prev += current.value), 0);
 
     return (
         <div className={classes.info_block}>
             {items.map((el, index) => (
-                <div className={classes.info_block__item}>
+                <div key={index} className={classes.info_block__item}>
                     <div
                         className={classes.info_block__circle}
                         style={{ backgroundColor: el.color }}
                     />
-                    <span className={classes.info_block__caption}>{`${el.name}: ${el.value} (${(
-                        (el.value / valueSum) *
-                        100
-                    ).toFixed(1)}%)`}</span>
+                    <span className={classes.info_block__caption}>
+                        {`${el.name}: ${el.value} (${((el.value / valueSum) * 100).toFixed(1)}%)`}
+                    </span>
                 </div>
             ))}
         </div>
